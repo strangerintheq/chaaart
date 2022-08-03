587e43d6d7e1a39ac7796c3a98fd1ae3bb5cb47c
k.balashev <k.balashev@geoscan.aero>
2022-08-03 17:41:20 +0300

    chaaart

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
     Object3D,
     PerspectiveCamera,
     Points,
-    PointsMaterial,
+    PointsMaterial, Raycaster,
     Scene, Shader,
     WebGLRenderer
 } from "three";
@@ -31,6 +31,10 @@ const renderer = new WebGLRenderer({
 })
 
 const scene = new Scene();
+const pointsObj= new Object3D();
+const linesObj= new Object3D();
+scene.add(pointsObj)
+scene.add(linesObj)
 scene.background = new Color().set('#fff')
 scene.add(new GridHelper(50,5))
 
@@ -41,7 +45,7 @@ camera.lookAt(scene.position)
 new OrbitControls(camera, canvas)
     .addEventListener('change', render);
 
-let axesHelper = new AxesHelper(30);
+const axesHelper = new AxesHelper(30);
 axesHelper.scale.z = -1
 axesHelper.setColors(
     new Color(1,0,0),
@@ -53,16 +57,16 @@ scene.add(axesHelper)
 document.querySelector('#file')
     .addEventListener('change',  processFile)
 
+canvas.addEventListener('click', ({clientX:x, clientY:y}) => {
+    const coords = {
+        x:x/innerWidth*2-1,
+        y:y/innerHeight*2-1
+    };
+    console.log(coords)
+})
+
 addSlider('#timeline-tMax', v => tMax.value = v)
 addSlider('#timeline-tMin', v => tMin.value = v)
-
-function addSlider(selector, f){
-    let timeline = document.querySelector(selector) as HTMLInputElement;
-    timeline.addEventListener('input', () => {
-        f(+timeline.value)
-        render();
-    });
-}
 
 resize()
 
@@ -73,10 +77,19 @@ function render() {
 }
 
 function resize() {
+    renderer.setPixelRatio(devicePixelRatio);
     renderer.setSize(innerWidth, innerHeight)
     camera.aspect = innerWidth / innerHeight
     camera.updateProjectionMatrix()
     render()
+}
+
+function addSlider(selector, f){
+    let timeline = document.querySelector(selector) as HTMLInputElement;
+    timeline.addEventListener('input', () => {
+        f(+timeline.value)
+        render();
+    });
 }
 
 function processFile(event){
@@ -86,22 +99,23 @@ function processFile(event){
 }
 
 function addData(data: string, file:string) {
-    // here
     data = data.substr(0, data.length-2) + ']'
-    const object3D = new Object3D()
+    const object3d = new Object3D()
     const dataArray = JSON.parse(data);
     const fields = ['p', 'a', 'v', 'v1', 'v2', 'v3']
     dataArray.forEach(p => fields.forEach(f => p[f] = convertPoint(p[f])))
     const color = colors[Math.floor(colors.length*Math.random())]
-    const add = (o) => object3D.add(o) && o
+    const add = (object) => object3d.add(object) && object
     const p =  add(createPointsCloud(dataArray.map(p => ({...p.p, t:p.t})), color, 0.3));
+    const t =  add(createTrajectory(dataArray.map(p => ({...p.p, t:p.t})), color));
     const a =  add(createPointsCloud(dataArray.map(p => ({...p.a, t:p.t})), '#0000ff', 0.5));
     const v1 = add(createVectors(dataArray.map(p => ({p0:p.p, p1:p.v1, t:p.t})), '#0000ff', 1));
     const v2 = add(createVectors(dataArray.map(p => ({p0:p.p, p1:p.v2, t:p.t})), '#00ff00', 1));
     const v3 = add(createVectors(dataArray.map(p => ({p0:p.p, p1:p.v3, t:p.t})), '#ff0000' ,1));
     const v =  add(createVectors(dataArray.map(p => ({p0:p.p, p1:p.v,  t:p.t})), '#ff00ff', 1));
     datasets.append(createDatasetControls())
-    scene.add(object3D)
+    pointsObj.add(object3d)
+
     render()
 
     function createDatasetControls() {
@@ -118,10 +132,11 @@ function addData(data: string, file:string) {
         let colorControl = datasetControls.querySelector('input[type="color"]') as HTMLInputElement;
         colorControl.addEventListener('input', () => {
             p.material.color.set(colorControl.value)
+            t.material.color.set(colorControl.value)
             render()
         })
 
-        addCheckbox('cb-visibility', x => object3D.visible = x)
+        addCheckbox('cb-visibility', x => object3d.visible = x)
         addCheckbox('cb-v1', x => v1.visible = x)
         addCheckbox('cb-v2', x => v2.visible = x)
         addCheckbox('cb-v3', x => v3.visible = x)
@@ -211,7 +226,7 @@ function createVectors(data: {p0, p1, t}[], colorHex, multiplier){
 
 }
 
-function createTrajectory(data: {p, t}[], colorHex){
+function createTrajectory(data: {x,y,z, t}[], colorHex){
     const material = new LineBasicMaterial({
         color: new Color().set(colorHex)
     });
@@ -221,12 +236,12 @@ function createTrajectory(data: {p, t}[], colorHex){
     for (let i = 0; i < data.length-1; i++) {
         const v1 = data[i]
         const v2 = data[i+1]
-        positions[i * s] = v1.p.x;
-        positions[i * s + 1] = v1.p.y;
-        positions[i * s + 2] = v1.p.z;
-        positions[i * s + 3] = v2.p.x;
-        positions[i * s + 4] = v2.p.y;
-        positions[i * s + 5] = v2.p.z;
+        positions[i * s] = v1.x;
+        positions[i * s + 1] = v1.y;
+        positions[i * s + 2] = v1.z;
+        positions[i * s + 3] = v2.x;
+        positions[i * s + 4] = v2.y;
+        positions[i * s + 5] = v2.z;
     }
     const times = data.map(p => [p.t, p.t]).flat();
     const geometry = new BufferGeometry();
@@ -236,7 +251,6 @@ function createTrajectory(data: {p, t}[], colorHex){
     return  new LineSegments(geometry, material);
 
 }
-
 
 function convertPoint(p){
     return {

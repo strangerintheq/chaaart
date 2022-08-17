f30d30ba42745bba3152b6f8775f2374613e4361
k.balashev <k.balashev@geoscan.aero>
2022-08-17 16:49:45 +0300

    react

--- src/react/cameraPosition.tsx
+++ src/react/cameraPosition.tsx
@@ -1,7 +1,6 @@
 import {useThree} from "@react-three/fiber";
-import {Vector3} from "three";
 
-export function CameraPosition({x, y, z}: Vector3) {
+export function CameraPosition({x, y, z}) {
     useThree(three => three.camera.position.set(x, y, z));
     return null
 }
